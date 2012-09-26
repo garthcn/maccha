2d217df038d07428b1c46ae804a4839bcc043646
Jiasi Zeng <garthcn@gmail.com>
2012-09-26 11:14:57 -0400

    add more test for Item model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,19 +2,50 @@ require 'spec_helper'
 
 describe Item do
 
-  describe "item attributes" do
-    before { 
-      @item = Item.create(name: "iPhone", category: "phone") 
+  before(:each) do
+    @attr = {
+      :name => "iPhone 7",
+      :category => "Electronics",
+      :price =>  "10.0",
+      :start_time => DateTime.new(2012,12,24,11,59,00),
+      :end_time => DateTime.new(2012,12,24,11,59,00),
+      :condition => "like new",
+      :location => "Pittsburgh",
+      :description => "android is better",
+      :seller_id => "1"
     }
-    subject { @user }
+    #@item = Factory(:item)
+  end
 
-    it { should respond_to(:name) }
-    it { should respond_to(:email) }
-    it { should respond_to(:encrypted_password) }
+  describe "item attributes" do
+    @item = Item.create(@attr)
+    subject { @item }
+
+    it.name.should == 'iPhone'
+    it.category.should == 'Electronics'
+    it.location.should == 'Pittsburgh'
   end
 
   describe "item CRUD" do
-  
+    it "should create an item" do
+      Item.create(@attr)
+      @item = Item.find_by_name('iPhone 7')
+      @item.should_not be_blank
+      @item.description.should == 'android is better'
+    end
+
+    it "should update an item attribute" do
+      @item = Item.create(@attr)
+      @attr['name'] = 'iPhone 3G'
+      @item.update_attributes(@attr)
+      @item.name.should == 'iPhone 3G'
+    end
+
+    it "should delete an item" do
+      @item = Item.create(@attr)
+      @item.destroy
+      Item.find_by_name('iPhone 7').should be_blank 
+    end
   end
 
 end
