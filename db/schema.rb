749101d60591cce81c654b9c9aa811eb5043a95d
Lily <ying.li@sv.cmu.edu>
2012-10-03 16:46:36 -0400

    email confirm v2

--- db/schema.rb
+++ db/schema.rb
@@ -41,10 +41,10 @@ ActiveRecord::Schema.define(:version => 20121003191005) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
+    t.string   :confirmation_token
+    t.datetime :confirmed_at
+    t.datetime :confirmation_sent_at
+    t.string   :unconfirmed_email
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
