395a2de946ef5f3a02d1be3e416b934108199e63
Lily <ying.li@sv.cmu.edu>
2012-10-03 16:10:47 -0400

    email confirm

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,21 @@ BestBay::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "gmail.com",
+      :authentication => "plain",
+      :enable_starttls_auto => true,
+      :user_name => "liyingCmu@gmail.com",
+      :password => "0814Cmu$"
+  }
 end
