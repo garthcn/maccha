b7e03516e66f19aa87ee6f5602747b871c6e1c74
Yang Piao <yp@cmu.edu>
2012-11-10 12:58:35 -0500

    Modified item#show.

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -24,31 +24,25 @@ class BidsController < ApplicationController
     #@bid_existing=Bid.find_by_item_id_and_buyer_id(params[:bid][:item_id],params[:bid][:buyer_id])
     @item = Item.find(params[:bid][:item_id])
     #if          @bid_existing.nil?
-
-
-
     #@bid = Bid.new(params[:bid])
-
     #else
-
     #end
     #  print "new #{current_user.id}"
-    if  ( !@item.bids.maximum(:price).nil?)
-
-      if params[:bid][:price].to_f <  @item.bids.maximum(:price)
-
-      else
-        @bid  =  @item.bids.build (params[:bid])
-
-      end
-
-
-    elsif
-       (  params[:bid][:price].to_f <  @item.price)
-
+    price = @item.bids.maximum(:price) || @item.price
+    params[:bid][:price] = params[:bid][:price].to_f.round(2)
+    if params[:bid][:price] < price
     else
-      @bid  =  @item.bids.build (params[:bid])
-      end
+      @bid = @item.bids.build(params[:bid])
+    end
+    # if  ( !@item.bids.maximum(:price).nil?)
+    #   if params[:bid][:price] < @item.bids.maximum(:price)
+    #   else
+    #     @bid  =  @item.bids.build (params[:bid])
+    #   end
+    # elsif params[:bid][:price] < @item.price
+    # else
+    #   @bid  =  @item.bids.build (params[:bid])
+    # end
     respond_to do |format|
       if  !@bid.nil? && @bid.save
         print " test1 #{@bid.attributes_before_type_cast["created_at"]}"
@@ -56,12 +50,12 @@ class BidsController < ApplicationController
         format.html { redirect_to @item, notice: 'Bid was successfully created.' }
         format.json { render json: @bid, status: :created, location: @bid }
       else
-        format.html {redirect_to @item, notice: 'Error! Bid was not created. Bid should be higher than current price!'  }
+        format.html { redirect_to @item, notice: 'Error! Bid was not created. Bid should be higher than current price!' }
         format.json { render json: @bid.errors, status: :unprocessable_entity }
       end
-
-      end
+    end
   end
+
   def update
     @bid_existing=Bid.find(params[:id])
     @item = Item.find(params[:bid][:item_id])
