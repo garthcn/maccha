b11dca2ff2b5161815daa556975bfc3a1ab7d1df
Lily <ying.li@sv.cmu.edu>
2012-11-10 14:39:25 -0500

    admin login v2

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,20 +1,17 @@
-class FakeUser
-  def self.username
-    'admin'
-  end
-
-  def self.email
-    'liyingcmu@gmail.com'
-  end
-end
 
 RailsAdmin.config do |config|
-  config.current_user_method do
-    authenticate_or_request_with_http_basic do |username, password|
-      username == "admin" && password == "adminpass"
-    end
-    FakeUser
+  #config.current_user_method do
+    #authenticate_or_request_with_http_basic do |username, password|
+     # username == "admin" && password == "adminpass"
+   # end
+  #  FakeUser
+ # end
+ # config.main_app_name { ['Recommended Games', 'Admin'] }
+  #config.authenticate_with{}
+  config.authorize_with do
+      authenticate_or_request_with_http_basic('Site Message') do |username, password|
+        username == 'admin' && password == 'adminpass'
+      end
   end
   config.main_app_name { ['Recommended Games', 'Admin'] }
-  config.authenticate_with{}
 end
